77c678bfb77acf9213efe846d292adf6e7b8ba10
eric-castillo05 <ericcastillo005@gmail.com>
2024-10-20 21:17:49 -0600

    refactor: implement singleton pattern for database connection

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -3,12 +3,22 @@ from firebase_admin import credentials, firestore
 from flask import Flask
 from flaskr.config import Config
 
+
+class FirestoreClient():
+    _instance = None
+
+    @classmethod
+    def get_instance(cls, cred_path):
+        if cls._instance is None:
+            cred = credentials.Certificate(cred_path)
+            firebase_admin.initialize_app(cred)
+            cls._instance = firestore.client()
+        return cls._instance
+
+
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
-    cred = credentials.Certificate(app.config['SECRET_KEY'])
-    firebase_admin.initialize_app(cred)
-
-    db = firestore.client()
+    db = FirestoreClient.get_instance(app.config['DATABASE'])
     app.config['db'] = db
     return app
